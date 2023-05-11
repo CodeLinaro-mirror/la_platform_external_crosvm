e4b5165818f7ccc4b6c51f20c95c397f89e8c699
Jianguo Sun <quic_jianguos@quicinc.com>
2023-05-11 21:06:13 +0800

    device: input: add support for flat device config
    Now virtualized-input takes stack device config by default, needs hypervisor to bank
    the config in stack mode, add support for flat device config in case hypervisor
    don't support stack mode or there is no hypervisor.
    
    Change-Id: Ifc11b478bcc8e2d0d626d3ffab7f1c37d1d04639

--- devices/src/virtio/mod.rs
+++ devices/src/virtio/mod.rs
@@ -7,7 +7,6 @@
 mod async_utils;
 mod balloon;
 mod descriptor_utils;
-mod input;
 mod interrupt;
 mod iommu;
 mod p9;
@@ -28,6 +27,7 @@ pub mod console;
 pub mod fs;
 #[cfg(feature = "gpu")]
 pub mod gpu;
+pub mod input;
 pub mod net;
 pub mod resource_bridge;
 #[cfg(feature = "audio")]
