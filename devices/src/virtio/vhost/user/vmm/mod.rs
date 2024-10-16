0243edf754db48d12be21f8205e36649bf8ef77d
Wei Liu <quic_wliu8@quicinc.com>
2024-10-15 19:12:02 -0700

    crosvm: Add vhost user SSR support in crosvm
    Add the vhost user SSR for the ssr protocol to support transport
    ssr events to gvm, over the vhost-user virtio.
    
    This patch only provides the vhost user frontend implementation.
    
    Change-Id: If746b28e12ebe335472fa15dab92adc22dba1e7b
    Signed-off-by: Wei Liu <quic_wliu8@quicinc.com>

--- devices/src/virtio/vhost/user/vmm/mod.rs
+++ devices/src/virtio/vhost/user/vmm/mod.rs
@@ -25,6 +25,7 @@ mod scmi;
 mod glink_cma;
 mod i2c;
 mod frpc;
+mod ssr;
 
 pub use self::block::*;
 pub use self::console::*;
@@ -43,6 +44,7 @@ pub use self::scmi::*;
 pub use self::glink_cma::*;
 pub use self::i2c::*;
 pub use self::frpc::*;
+pub use self::ssr::*;
 
 use remain::sorted;
 use thiserror::Error as ThisError;
