a6caac37d0e4b27c5859ca4599e90c203ccd217f
Andriy Tryshnivskyy <atryshni@qti.qualcomm.com>
2025-10-21 11:13:03 +0200

    Correct licenses in vhost-user generic device sources.
    Change-Id: I39abbf8c34fa93c2c08a25886ee55139b5fa6e6a

--- devices/src/virtio/vhost/user/vmm/mod.rs
+++ devices/src/virtio/vhost/user/vmm/mod.rs
@@ -2,10 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-//Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
-
-//Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
-//SPDX-License-Identifier: BSD-3-Clause-Clear
+// Changes from Qualcomm Technologies, Inc. are provided under the following license:
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
 
 mod block;
 mod console;
